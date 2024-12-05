97750402e036c8179bd95c6c6d03f73e2e888df8
max <amgine.gitter@gmail.com>
2024-12-05 08:27:08 +0300

    added day 5

---
+++ 2024/day05/Solution.cs
@@ -0,0 +1,114 @@
+﻿
+namespace AoC.Year2024;
+
+/// <remarks><a href="https://adventofcode.com/2024/day/5"/></remarks>
+[Name(@"XXX")]
+public abstract class Day05Solution : Solution
+{
+	protected readonly record struct Dependency(int Page, int Before);
+
+	protected sealed class Input
+	{
+		public HashSet<Dependency> Rules { get; } = [];
+
+		public List<int[]> PrintQueues { get; } = [];
+	}
+
+	protected static bool IsValid(HashSet<Dependency> rules, int[] queue)
+	{
+		for(int i = 0; i < queue.Length - 1; ++i)
+		{
+			for(int j = i + 1; j < queue.Length; ++j)
+			{
+				if(rules.Contains(new(queue[j], queue[i])))
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public sealed override string Process(TextReader reader)
+	{
+		var input = new Input();
+		var deps = true;
+		string? line;
+		while((line = reader.ReadLine()) is not null)
+		{
+			if(string.IsNullOrEmpty(line))
+			{
+				if(deps)
+				{
+					deps = false;
+				}
+				continue;
+			}
+
+			if(deps)
+			{
+				var p = line.Split('|');
+				input.Rules.Add(new(int.Parse(p[0]), int.Parse(p[1])));
+			}
+			else
+			{
+				input.PrintQueues.Add(Array.ConvertAll(line.Split(','), int.Parse));
+			}
+		}
+
+		return Solve(input).ToString();
+	}
+
+	protected abstract int Solve(Input input);
+}
+
+public sealed class Day05SolutionPart1 : Day05Solution
+{
+	protected override int Solve(Input input)
+	{
+		var sum = 0;
+		foreach(var queue in input.PrintQueues)
+		{
+			if(!IsValid(input.Rules, queue)) continue;
+			sum += queue[queue.Length / 2];
+		}
+		return sum;
+	}
+}
+
+public sealed class Day05SolutionPart2 : Day05Solution
+{
+	private static int[] MakeValid(HashSet<Dependency> rules, int[] queue)
+	{
+		var valid = (int[])queue.Clone();
+		do
+		{
+			for(int i = 0; i < valid.Length - 1; ++i)
+			{
+				for(int j = i + 1; j < valid.Length; ++j)
+				{
+					if(rules.Contains(new(valid[j], valid[i])))
+					{
+						(valid[j], valid[i]) = (valid[i], valid[j]);
+						break;
+					}
+				}
+			}
+		}
+		while(!IsValid(rules, valid));
+
+		return valid;
+	}
+
+	protected override int Solve(Input input)
+	{
+		var sum = 0;
+		foreach(var order in input.PrintQueues)
+		{
+			if(IsValid(input.Rules, order)) continue;
+			var valid = MakeValid(input.Rules, order);
+			sum += valid[valid.Length / 2];
+		}
+		return sum;
+	}
+}
---
+++ tests/2024/Day05Test.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace AoC.Year2024;
+
+[TestFixture]
+class Day05Test
+{
+	const string SampleInput1 =
+		"""
+		47|53
+		97|13
+		97|61
+		97|47
+		75|29
+		61|13
+		75|53
+		29|13
+		97|29
+		53|29
+		61|53
+		97|53
+		61|29
+		47|13
+		75|47
+		97|75
+		47|61
+		75|61
+		47|29
+		75|13
+		53|13
+
+		75,47,61,53,29
+		97,61,53,29,13
+		75,29,13
+		75,97,47,61,53
+		61,13,29
+		97,13,75,29,47
+		""";
+
+	[Test]
+	public void SolvePart1() => Helper.ValidateSolution<Day05SolutionPart1>(
+		expected: "143", input: SampleInput1);
+
+	[Test]
+	public void SolvePart2() => Helper.ValidateSolution<Day05SolutionPart2>(
+		expected: "123", input: SampleInput1);
+}
