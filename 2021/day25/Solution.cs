a99592b72c5ad32545fed23b08b72073ac0227bb
max <amgine.gitter@gmail.com>
2024-12-09 10:02:05 +0300

    added 2021 day 25

---
+++ 2021/day25/Solution.cs
@@ -0,0 +1,66 @@
+﻿namespace AoC.Year2021;
+
+[Name(@"Sea Cucumber")]
+public abstract class Day25Solution : Solution
+{
+}
+
+public sealed class Day25SolutionPart1 : Day25Solution
+{
+	static bool MoveEast(char[,] map)
+	{
+		var toMove = new List<Point2D>();
+		for(int y = 0; y < map.GetLength(0); ++y)
+		{
+			for(int x = 0; x < map.GetLength(1); ++x)
+			{
+				if(map[y, x] != '>') continue;
+				var next = (x + 1) % map.GetLength(1);
+				if(map[y, next] != '.') continue;
+				toMove.Add(new Point2D(x, y));
+			}
+		}
+		if(toMove.Count == 0) return false;
+		foreach(var p in toMove)
+		{
+			p.GetValue(map) = '.';
+			var next = (p.X + 1) % map.GetLength(1);
+			map[p.Y, next] = '>';
+		}
+		return true;
+	}
+
+	static bool MoveNorth(char[,] map)
+	{
+		var toMove = new List<Point2D>();
+		for(int y = 0; y < map.GetLength(0); ++y)
+		{
+			for(int x = 0; x < map.GetLength(1); ++x)
+			{
+				if(map[y, x] != 'v') continue;
+				var next = (y + 1) % map.GetLength(0);
+				if(map[next, x] != '.') continue;
+				toMove.Add(new Point2D(x, y));
+			}
+		}
+		if(toMove.Count == 0) return false;
+		foreach(var p in toMove)
+		{
+			p.GetValue(map) = '.';
+			var next = (p.Y + 1) % map.GetLength(0);
+			map[next, p.X] = 'v';
+		}
+		return true;
+	}
+
+	public override string Process(TextReader reader)
+	{
+		var map = LoadCharMap2D(reader);
+		var steps = 1;
+		while(MoveEast(map) | MoveNorth(map))
+		{
+			++steps;
+		}
+		return steps.ToString();
+	}
+}
---
+++ tests/2021/Day25Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace AoC.Year2021;
+
+[TestFixture]
+class Day25Test
+{
+	const string SampleInput1 =
+		"""
+		v...>>.vv>
+		.vv>>.vv..
+		>>.>v>...v
+		>>v>>.>.v.
+		v>v.vv.v..
+		>.>>..v...
+		.vv..>.>v.
+		v.v..>>v.v
+		....v..v.>
+		""";
+
+	[Test]
+	public void SolvePart1() => Helper.ValidateSolution<Day25SolutionPart1>(
+		expected: "58", input: SampleInput1);
+}
