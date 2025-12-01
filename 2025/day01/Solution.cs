00f472da092fa1432e788cc73bcf6f5054b253d4
max <amgine.gitter@gmail.com>
2025-12-01 08:36:34 +0300

    added 2025 day 1

---
+++ 2025/day01/Solution.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+
+namespace AoC.Year2025;
+
+/// <remarks><a href="https://adventofcode.com/2025/day/1"/></remarks>
+[Name(@"Secret Entrance")]
+public abstract class Day01Solution : Solution
+{
+	protected const int InitialDial = 50;
+
+	protected static List<int> ReadInput(TextReader reader)
+	{
+		var offsets = new List<int>();
+		string? line;
+		while((line = reader.ReadLine()) is not null)
+		{
+			if(string.IsNullOrWhiteSpace(line)) continue;
+			var sign = line[0] switch
+			{
+				'L' => -1,
+				'R' =>  1,
+				_ => throw new InvalidDataException(),
+			};
+			var value = int.Parse(line.AsSpan(1), NumberStyles.Integer, CultureInfo.InvariantCulture);
+			offsets.Add(sign * value);
+		}
+		return offsets;
+	}
+
+	public override string Process(TextReader reader)
+		=> Solve(ReadInput(reader)).ToString(CultureInfo.InvariantCulture);
+
+	protected abstract int Solve(List<int> offsets);
+}
+
+public sealed class Day01SolutionPart1 : Day01Solution
+{
+	protected override int Solve(List<int> offsets)
+	{
+		var password = 0;
+		var dial = InitialDial;
+		foreach(var offset in offsets)
+		{
+			dial += offset;
+			while(dial < 0) dial += 100;
+			dial %= 100;
+			if(dial == 0) ++password;
+		}
+		return password;
+	}
+}
+
+public sealed class Day01SolutionPart2 : Day01Solution
+{
+	protected override int Solve(List<int> offsets)
+	{
+		var password = 0;
+		var dial = InitialDial;
+		foreach(var offset in offsets)
+		{
+			var wasZero = dial == 0;
+			dial += offset;
+			if(dial == 0)
+			{
+				++password;
+			}
+			else if(dial > 0)
+			{
+				password += dial / 100;
+				dial %= 100;
+			}
+			else
+			{
+				if(!wasZero) ++password;
+				password -= dial / 100;
+				while(dial < 0) dial += 100;
+			}
+		}
+		return password;
+	}
+}
---
+++ tests/2025/Day01Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace AoC.Year2025;
+
+[TestFixture]
+class Day05Test
+{
+	const string SampleInput1 =
+		"""
+		L68
+		L30
+		R48
+		L5
+		R60
+		L55
+		L1
+		L99
+		R14
+		L82
+		""";
+
+	[Test]
+	public void SolvePart1() => Helper.ValidateSolution<Day01SolutionPart1>(
+		expected: "3", input: SampleInput1);
+
+	[Test]
+	public void SolvePart2() => Helper.ValidateSolution<Day01SolutionPart2>(
+		expected: "6", input: SampleInput1);
+
+	[Test]
+	public void SolvePart2Case1() => Helper.ValidateSolution<Day01SolutionPart2>(
+		expected: "10", input: "R1000");
+}
