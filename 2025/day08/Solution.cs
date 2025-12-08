d54651ab6638143e60c735824dea5d1fb7b396e6
max <amgine.gitter@gmail.com>
2025-12-08 09:11:18 +0300

    added 2025 day 8

---
+++ 2025/day08/Solution.cs
@@ -0,0 +1,126 @@
+﻿namespace AoC.Year2025;
+
+/// <remarks><a href="https://adventofcode.com/2025/day/8"/></remarks>
+[Name(@"Playground")]
+public abstract class Day08Solution : Solution
+{
+	protected static long DistanceSquared(Point3D p0, Point3D p1)
+	{
+		var dx = (long)(p1.X - p0.X);
+		var dy = (long)(p1.Y - p0.Y);
+		var dz = (long)(p1.Z - p0.Z);
+		return dx * dx + dy * dy + dz * dz;
+	}
+
+	protected class Circuit
+	{
+		public List<Node> Nodes { get; } = [];
+	}
+
+	protected class Node(Point3D coordinates)
+	{
+		public Point3D Coordinates { get; } = coordinates;
+
+		public Circuit? Circuit { get; set; }
+
+		public override string ToString() => Coordinates.ToString();
+	}
+
+	protected static List<(Node A, Node B, long Distance)> LoadInput(TextReader reader, out List<Node> nodes)
+	{
+		    nodes     = LoadListFromNonEmptyStrings(reader, static line => new Node(Parsers.ParsePoint3D(line)));
+		var circuits  = new List<Circuit>();
+		var distances = new List<(Node A, Node B, long Distance)>();
+
+		for(int i = 0; i < nodes.Count - 1; ++i)
+		{
+			for(int j = i + 1; j < nodes.Count; ++j)
+			{
+				var a = nodes[i];
+				var b = nodes[j];
+				var distance = DistanceSquared(a.Coordinates, b.Coordinates);
+				distances.Add((a, b, distance));
+			}
+		}
+		distances.Sort(static (a, b) => a.Distance.CompareTo(b.Distance));
+		return distances;
+	}
+
+	protected static void Connect(List<Circuit> circuits, Node a, Node b)
+	{
+		if(a.Circuit is null && b.Circuit is null)
+		{
+			var c = new Circuit();
+			c.Nodes.Add(a);
+			c.Nodes.Add(b);
+			a.Circuit = c;
+			b.Circuit = c;
+			circuits.Add(c);
+		}
+		else if(a.Circuit is not null && b.Circuit is null)
+		{
+			b.Circuit = a.Circuit;
+			a.Circuit.Nodes.Add(b);
+		}
+		else if(b.Circuit is not null && a.Circuit is null)
+		{
+			a.Circuit = b.Circuit;
+			b.Circuit.Nodes.Add(a);
+		}
+		else if(a.Circuit is not null && b.Circuit is not null)
+		{
+			if(a.Circuit == b.Circuit) return;
+			circuits.Remove(b.Circuit);
+			a.Circuit.Nodes.AddRange(b.Circuit.Nodes);
+			foreach(var node in b.Circuit.Nodes)
+			{
+				node.Circuit = a.Circuit;
+			}
+		}
+	}
+}
+
+public sealed class Day08SolutionPart1 : Day08Solution
+{
+	public static int Solve(TextReader reader, int connections)
+	{
+		var circuits  = new List<Circuit>();
+		var distances = LoadInput(reader, out _);
+
+		foreach(var (a, b, _) in distances)
+		{
+			if(connections-- <= 0) break;
+			Connect(circuits, a, b);
+		}
+
+		return circuits
+			.OrderByDescending(c => c.Nodes.Count)
+			.Take(3)
+			.Aggregate(1, static (m, c) => m * c.Nodes.Count);
+	}
+
+	public override string Process(TextReader reader)
+		=> Solve(reader, connections: 1000).ToString();
+}
+
+public sealed class Day08SolutionPart2 : Day08Solution
+{
+	static long GetAnswer(Node a, Node b)
+		=> (long)a.Coordinates.X * (long)b.Coordinates.X;
+
+	public override string Process(TextReader reader)
+	{
+		var circuits  = new List<Circuit>();
+		var distances = LoadInput(reader, out var nodes);
+
+		foreach(var (a, b, _) in distances)
+		{
+			Connect(circuits, a, b);
+			if(circuits.Count == 1 && circuits[0].Nodes.Count == nodes.Count)
+			{
+				return GetAnswer(a, b).ToString();
+			}
+		}
+		throw new InvalidDataException();
+	}
+}
---
+++ tests/2025/Day08Test.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace AoC.Year2025;
+
+[TestFixture]
+class Day08Test
+{
+	const string SampleInput1 =
+		"""
+		162,817,812
+		57,618,57
+		906,360,560
+		592,479,940
+		352,342,300
+		466,668,158
+		542,29,236
+		431,825,988
+		739,650,466
+		52,470,668
+		216,146,977
+		819,987,18
+		117,168,530
+		805,96,715
+		346,949,466
+		970,615,88
+		941,993,340
+		862,61,35
+		984,92,344
+		425,690,689
+		""";
+
+	[Test]
+	public void SolvePart1()
+		=> Assert.That(Day08SolutionPart1.Solve(new StringReader(SampleInput1), 10), Is.EqualTo(40));
+
+	[Test]
+	public void SolvePart2() => Helper.ValidateSolution<Day08SolutionPart2>(
+		expected: "25272", input: SampleInput1);
+}
