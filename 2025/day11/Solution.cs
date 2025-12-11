e45880d754277362967f967b32cd9822e358c5c8
max <amgine.gitter@gmail.com>
2025-12-11 09:24:30 +0300

    added 2025 day 11

---
+++ 2025/day11/Solution.cs
@@ -0,0 +1,166 @@
+﻿namespace AoC.Year2025;
+
+/// <remarks><a href="https://adventofcode.com/2025/day/11"/></remarks>
+[Name(@"Reactor")]
+public abstract class Day11Solution : Solution
+{
+	protected sealed class Node(string name)
+	{
+		public string Name { get; } = name;
+
+		public List<Node> Outputs { get; } = [];
+
+		public List<Node> Inputs { get; } = [];
+
+		public override string ToString() => Name;
+	}
+
+	protected static Node GetNode(Dictionary<string, Node> nodes, string name)
+	{
+		if(!nodes.TryGetValue(name, out var node))
+		{
+			nodes.Add(name, node = new(name));
+		}
+		return node;
+	}
+
+	protected class Graph
+	{
+		private readonly List<Node> _nodes = [];
+		private readonly Dictionary<string, Node> _lookup = [];
+
+		public Node this[string name] => _lookup[name];
+
+		public Node GetOrCreateNode(string name)
+		{
+			if(!_lookup.TryGetValue(name, out var node))
+			{
+				_lookup.Add(name, node = new(name));
+				_nodes.Add(node);
+			}
+			return node;
+		}
+
+		public void Simplify(Predicate<Node>? preserve = default)
+		{
+			bool changed;
+			do
+			{
+				changed = false;
+				for(int i = _nodes.Count - 1; i >= 0; --i)
+				{
+					var n = _nodes[i];
+					if(preserve is not null && preserve(n))
+					{
+						continue;
+					}
+					if(n.Inputs.Count == 1 && n.Outputs.Count == 1)
+					{
+						var input = n.Inputs[0];
+						var output = n.Outputs[0];
+						input.Outputs.Remove(n);
+						input.Outputs.Add(output);
+						output.Inputs.Remove(n);
+						output.Inputs.Add(input);
+						_nodes.RemoveAt(i);
+						changed = true;
+					}
+				}
+			}
+			while(changed);
+		}
+	}
+
+	protected static Graph LoadGraph(TextReader reader)
+	{
+		var graph = new Graph();
+
+		string? line;
+		while((line = reader.ReadLine()) is not null)
+		{
+			if(string.IsNullOrWhiteSpace(line)) continue;
+
+			var s = line.IndexOf(':');
+			var name = line.Substring(0, s);
+			var node = graph.GetOrCreateNode(name);
+			var outs = line.Substring(s + 1).Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+			foreach(var o in outs)
+			{
+				var n = graph.GetOrCreateNode(o);
+				node.Outputs.Add(n);
+				n.Inputs.Add(node);
+			}
+		}
+
+		return graph;
+	}
+}
+
+public sealed class Day11SolutionPart1 : Day11Solution
+{
+	static long FindPaths(Node pos, Node dst)
+	{
+		if(pos == dst) return 1;
+
+		var count = 0L;
+		foreach(var node in pos.Outputs)
+		{
+			count += FindPaths(node, dst);
+		}
+		return count;
+	}
+
+	public override string Process(TextReader reader)
+	{
+		var graph = LoadGraph(reader);
+		graph.Simplify(static n => n.Name is @"you" or @"out");
+		return FindPaths(graph[@"you"], graph[@"out"]).ToString();
+	}
+}
+
+public sealed class Day11SolutionPart2 : Day11Solution
+{
+	static long FindPaths(Node pos, Node dst, Predicate<Node>? filter = default)
+	{
+		if(pos == dst) return 1;
+
+		var count = 0L;
+		foreach(var next in pos.Outputs)
+		{
+			if(filter is not null && !filter(next)) continue;
+			count += FindPaths(next, dst, filter);
+		}
+		return count;
+	}
+
+	private static HashSet<Node> ReachableFrom(Node node)
+	{
+		var reachableFrom = new HashSet<Node>() { node };
+		var queue = new Queue<Node>();
+		queue.Enqueue(node);
+		while(queue.TryDequeue(out var n))
+		{
+			foreach(var input in n.Inputs)
+			{
+				if(reachableFrom.Add(input)) queue.Enqueue(input);
+			}
+		}
+		return reachableFrom;
+	}
+
+	public override string Process(TextReader reader)
+	{
+		var graph = LoadGraph(reader);
+
+		graph.Simplify(static n => n.Name is @"svr" or @"fft" or @"dac" or @"out");
+
+		var start  = graph[@"svr"];
+		var finish = graph[@"out"];
+
+		var x = FindPaths(graph[@"svr"], graph[@"fft"], ReachableFrom(graph[@"fft"]).Contains);
+		var y = FindPaths(graph[@"fft"], graph[@"dac"], ReachableFrom(graph[@"dac"]).Contains);
+		var z = FindPaths(graph[@"dac"], graph[@"out"]);
+
+		return (x * y * z).ToString();
+	}
+}
---
+++ tests/2025/Day11Test.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace AoC.Year2025;
+
+[TestFixture]
+class Day11Test
+{
+	const string SampleInput1 =
+		"""
+		aaa: you hhh
+		you: bbb ccc
+		bbb: ddd eee
+		ccc: ddd eee fff
+		ddd: ggg
+		eee: out
+		fff: out
+		ggg: out
+		hhh: ccc fff iii
+		iii: out
+		""";
+
+	const string SampleInput2 =
+		"""
+		svr: aaa bbb
+		aaa: fft
+		fft: ccc
+		bbb: tty
+		tty: ccc
+		ccc: ddd eee
+		ddd: hub
+		hub: fff
+		eee: dac
+		dac: fff
+		fff: ggg hhh
+		ggg: out
+		hhh: out
+		""";
+
+	[Test]
+	public void SolvePart1() => Helper.ValidateSolution<Day11SolutionPart1>(
+		expected: "5", input: SampleInput1);
+
+	[Test]
+	public void SolvePart2() => Helper.ValidateSolution<Day11SolutionPart2>(
+		expected: "2", input: SampleInput2);
+}
